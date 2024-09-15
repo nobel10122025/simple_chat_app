738c5f86979e9bfa1508f4ea5a18cdf3e8a5d021
Jaya Durga V <nobel10122025@gmail.com>
2024-09-15 11:26:21 +0530

    added groq to train model

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.documents import Document
 from langchain.chains import create_retrieval_chain
-
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+groq_api_key=os.getenv("GROQ_API_KEY")
 
 embeddings = OllamaEmbeddings(
     model="mxbai-embed-large",
 )
 vectorstoredb_local = FAISS.load_local("faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True)
-llm=Ollama(model="gemma2:2b")
+llm=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+load_dotenv() ## loading all the environment variable
+
 
 ##prompts
 prompt=ChatPromptTemplate.from_template(
@@ -32,7 +38,7 @@ retriever=vectorstoredb_local.as_retriever()
 retrieval_chain=create_retrieval_chain(retriever,document_chain)
 
 ## streamlit framework
-st.title("Langchain Demo With Gemma Model")
+st.title("Astrology chat bot")
 input_text=st.text_input("What question you have in mind?")
 
 
